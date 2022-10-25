deed207f7aae3a913d7c7862f958575dad0caaa3
Md Adit Hasan <skadithasan19@gmail.com>
2022-10-24 22:44:51 -0500

    Initial Commit

---
+++ Comcast-Test/Comcast_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Comcast_TestApp.swift
+//  Comcast-Test
+//
+//  Created by Adit Hasan on 10/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Comcast_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
